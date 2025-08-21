f60886b0e5bfef119ab8d0f944e7b6ac033eed34
anamaiberg <anamaiberg1776@gmail.com>
2025-08-20 21:45:25 -0300

    contador de clicks

--- app/src/main/java/com/example/exerciciosofc/MainActivity.java
+++ app/src/main/java/com/example/exerciciosofc/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.exerciciosofc;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView texto = findViewById(R.id.texto);
+        Button botao = findViewById(R.id.botao);
+
+        botao.setOnClickListener(v-> {
+            contador++;
+            texto.setText("Contagem: " + contador);
         });
     }
 }
