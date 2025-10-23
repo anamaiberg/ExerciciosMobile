ad4eab8a97f7c10890fd3676058b8e1df54c0615
Ana <anamaiberg1776@gmail.com>
2025-10-22 22:05:32 -0300

    paint semi-terminado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.github.skydoves:colorpickerview:2.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
